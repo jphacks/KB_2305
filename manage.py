4e1024bbbbe060d79182ca605e9c7d45659ce540
ImagawaKosuke <kosuke8de@gmail.com>
2023-10-29 13:58:16 +0900

    last

---
+++ manage.py
@@ -0,0 +1,89 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+from flask import Flask,render_template, request
+
+
+cred = credentials.Certificate("kb2305-5793d-firebase-adminsdk-vtb9d-db56bdc49e.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://kb2305-5793d-default-rtdb.firebaseio.com/',
+    'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
+})
+app = Flask(__name__, static_folder='/')
+
+@app.route("/")
+def hello():
+    ##databaseに初期データを追加する
+    modify_data = []
+    users_ref = db.reference('/Todos')
+    data = users_ref.get()
+    if data !=None:
+        for i in data:
+            print(str(i))
+            unmodify_data = [i['name'],i['todo'],int(i['matrix']),i['category'],i['start'],i['finish'],i['deadline'],i['option']]
+            modify_data.append(unmodify_data)
+    return render_template("index.html", data = modify_data)
+
+@app.route('/', methods=["GET", "POST"])
+def require():
+    modify_data = []
+    if request.method == 'GET': 
+        name = ""
+        post_todo = ""
+        post_matrix = ""
+        post_deadline = ""
+        post_start = ""
+        post_finish = ""
+        post_category = ""
+        post_option = ""
+        
+    elif request.method == 'POST':
+        name = request.form.get('name') #完了
+        post_todo = request.form.get('todo') #完了
+        post_matrix = request.form.get('matrix') #完了
+        post_deadline = str(request.form.get('deadline'))
+        post_start = str(request.form.get('start'))
+        post_finish = str(request.form.get('finish'))
+        post_category = request.form.get('category')
+        post_option = request.form.get('option')
+        
+        users_ref = db.reference('/Todos')
+        data = users_ref.get()
+        if data !=None:
+            num = str(len(data))
+            users_ref.child(num).set({
+                    'name': name,
+                    'todo': post_todo,
+                    'matrix': post_matrix,
+                    'deadline': post_deadline,
+                    'start': post_start,
+                    'finish': post_finish,
+                    'category': post_category,
+                    'option': post_option
+                
+                })
+        else:
+            users_ref.child('0').set({
+                    'name': name,
+                    'todo': post_todo,
+                    'matrix': post_matrix,
+                    'deadline': post_deadline,
+                    'start': post_start,
+                    'finish': post_finish,
+                    'category': post_category,
+                    'option': post_option
+                
+                })
+        data = users_ref.get()
+        for i in data:
+            print(str(i))
+            unmodify_data = [i['name'],i['todo'],int(i['matrix']),i['category'],i['start'],i['finish'],i['deadline'],i['option']]
+            modify_data.append(unmodify_data)
+    return render_template('index.html', data = modify_data) 
+
+##データを取得する
+
+app.run(debug=True)
